cfeff84a4c172f1bf56b345751b33ca208373c7d
fede <win-notebook>
2021-05-17 16:24:45 -0300

    init + primeNg + firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDW2P5T5UqP9xWuATwzbWSXi8U8nKZgbvA",
+    authDomain: "tp-final-labo-iv.firebaseapp.com",
+    projectId: "tp-final-labo-iv",
+    storageBucket: "tp-final-labo-iv.appspot.com",
+    messagingSenderId: "483373215600",
+    appId: "1:483373215600:web:8c9cc71d9d83f115e1a9b5"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
